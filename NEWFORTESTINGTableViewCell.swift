63ba67d98b5c551680a24ba7b6e43a20f42329e6
praveen <p8125802110@gmail.com>
2017-06-23 12:24:55 +0530

    update

---
+++ NEWFORTESTINGTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NEWFORTESTINGTableViewCell.swift
+//  settingMapkit
+//
+//  Created by Sridevi Voleti on 23/06/17.
+//  Copyright © 2017 Sridevi Voleti. All rights reserved.
+//
+
+import UIKit
+
+class NEWFORTESTINGTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
